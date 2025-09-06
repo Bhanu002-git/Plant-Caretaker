4b6d2b49381bc68effdb2c062ae70474bfdb36ef
Bhanu002-git <bpr2273@gmail.com>
2025-09-06 23:22:29 +0530

    Update plantCT.ino.ino

--- plantCT.ino.ino
+++ plantCT.ino.ino
@@ -1,37 +1,25 @@
+
+const int sensors[] = {6, 5, 12, 13};
+const int relays[]  = {3, 9, 10, 11};
+
 void setup() {
-  // put your setup code here, to run once:
-  int water; 
-    pinMode(3,OUTPUT); 
-    pinMode(11,OUTPUT); 
-    pinMode(9,OUTPUT); 
-    pinMode(10,OUTPUT);
-     pinMode(6,INPUT); 
-     pinMode(5,INPUT); 
-     pinMode(12,INPUT);
-     pinMode(13,INPUT);
+  for (int i=0; i<4; ++i) {
+    pinMode(sensors[i], INPUT);
+    pinMode(relays[i], OUTPUT);
+    digitalWrite(relays[i], HIGH); // ensure relays OFF initially (active LOW)
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-   int water = digitalRead(6);
-    if(water == HIGH) { 
-      digitalWrite(3,LOW); } 
-      
-      else { digitalWrite(3,HIGH); } 
-      delay(400); 
-      water = digitalRead(5); 
-      if(water == HIGH) { 
-        digitalWrite(9,LOW); } 
-        else { digitalWrite(9,HIGH); } 
-        delay(400); 
-        water = digitalRead(12); 
-        if(water == HIGH) { 
-          digitalWrite(10,LOW); } 
-          else { digitalWrite(10,HIGH); } 
-          delay(400); 
-          water = digitalRead(13); 
-          if(water == HIGH) { 
-            digitalWrite(11,LOW); } 
-            else { digitalWrite(11,HIGH); } 
-            delay(400); }
+  for (int i=0; i<4; ++i) {
+    int water = digitalRead(sensors[i]); // HIGH = wet, LOW = dry (typical FC-28 D0)
+    if (water == LOW) {                 // soil is dry -> turn pump ON
+      digitalWrite(relays[i], LOW);     // active LOW: LOW = relay ON
+    } else {                            // soil wet -> turn pump OFF
+      digitalWrite(relays[i], HIGH);    // HIGH = relay OFF
+    }
+    delay(400);
+  }
+}
+
 
